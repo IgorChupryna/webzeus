c41f0422f388d33c6bdb97ebf21a430e83cded16
IgorChupryna <chuprina.ihor@gmail.com>
2018-10-30 11:06:23 +0200

    first commit

---
+++ src/main/java/utils/Mountain.java
@@ -0,0 +1,19 @@
+package utils;
+
+/**
+ * Created by aleksey on 05.12.17.
+ */
+public class Mountain {
+
+        String name = "Himalaya";
+        static Mountain getMountain() {
+            System.out.println("Getting Name ");
+            return null;
+        }
+        public static void main(String[ ] args) {
+            Integer a = 5600;
+            Integer b = 5600;
+            System.out.println(a.equals(b));
+        }
+
+}
